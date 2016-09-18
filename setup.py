aa079ca219b48b54d7e4f7466cdf3c3ea72d9662
kengz <kengzwl@gmail.com>
2016-09-18 18:12:06 -0400

    fix pep8, indent 4

--- setup.py
+++ setup.py
@@ -13,17 +13,17 @@ test_args = [
 
 
 class PyTest(TestCommand):
-  user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
-  def initialize_options(self):
-    TestCommand.initialize_options(self)
-    self.pytest_args = test_args
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = test_args
 
-  def run_tests(self):
-    # import here, cause outside the eggs aren't loaded
-    import pytest
-    errno = pytest.main(self.pytest_args)
-    sys.exit(errno)
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(name='farming',
